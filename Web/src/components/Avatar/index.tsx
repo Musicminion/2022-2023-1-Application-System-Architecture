6995e6271009c45ab6c47e359f16a3cbd0e0d57d
zhangziqian <84625273+Musicminion@users.noreply.github.com>
2023-07-30 19:04:47 +0800

    Update index.tsx

--- Web/src/components/Avatar/index.tsx
+++ Web/src/components/Avatar/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import mystyle from './style.module.css';
 import clsx from 'clsx';
+import config from '@generated/docusaurus.config';
 
 export default function MainAvatar() {
     return (
         <div>
-            <img className={mystyle.MainAvatar} src="/img/logo-avatar.png" alt="ayaka" />
+            <img className={mystyle.MainAvatar} src={config.baseUrl + "img/logo-avatar.png"} alt="ayaka" />
         </div>
     );
 }
